1cfa49d4abf8e5a4b418566c3574b25d1b77216a
Winux2k <64943188+Winux2k@users.noreply.github.com>
2022-12-23 08:04:37 -0500

    Add files via upload

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,51 +3,8 @@ using Test
 
 @testset "FiniteDifferenceFormula.jl" begin
 	# confirmed
-	computecoefs(1, 0:1, true)
-	computecoefs(1, 0:2, true)
-	computecoefs(1, -1:0, true)
-	computecoefs(1, -2:0, true)
-	computecoefs(1, -1:1, true)
-	computecoefs(1, -2:2, true)
-	computecoefs(2, 0:2, true)
-	computecoefs(2, 0:3, true)
-	computecoefs(2, -2:0, true)
-	computecoefs(2, -3:0, true)
-	computecoefs(2, -1:1, true)
-	computecoefs(2, -2:2, true)
-	computecoefs(3, 0:3, true)
-	computecoefs(3, 0:4, true)
-	computecoefs(3, -3:0, true)
-	computecoefs(3, -4:0, true)
-	computecoefs(3, -2:2, true)
-	computecoefs(3, -3:3, true)
-	computecoefs(4, 0:4, true)
-	computecoefs(4, 0:5, true)
-	computecoefs(4, -4:0, true)
-	computecoefs(4, -5:0, true)
-	computecoefs(4, -2:2, true)
-	computecoefs(4, -3:3, true)
-	computecoefs(2, -3:3, true)
-	computecoefs(2, -4:4, true)
-
-	# for fun or exploring
-	computecoefs(1, -2:5, true)
-	computecoefs(2, -4:1)
-	formula()
-
-	# Doesn't work bc f^(4)(x[i]) term survives? More points are needed!
-	computecoefs(5, -3:1, true)
-	computecoefs(5, -2:2, true)
-	computecoefs(5, 0:4, true)
-
-	computecoefs(5, 0:5, true)
-	computecoefs(1, 2:5, true)
-	computecoefs(1, -5:-2, true)
-	computecoefs(1, -6:6, true)
-	computecoefs(2, -5:5, true)
-	computecoefs(3, -6:6, true)
-	computecoefs(8, -5:5, true)
-	computecoefs(10, -5:5)
-	formula()
-	computecoefs(2, -3:8, true)
+	@test sum(computecoefs(1, 0:1)[1]) == 0
+	@test sum(computecoefs(1, 0:2)[1]) == 0
+	@test sum(computecoefs(3, -2:5)[1]) == 0
+	@test computecoefs(3, -2:5)[2] > 0
 end
