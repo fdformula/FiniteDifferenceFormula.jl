64f48653b21657a8de2c6990008a5b9b0c2badfd
Winux2k <64943188+Winux2k@users.noreply.github.com>
2022-12-23 13:56:46 -0500

    Add files via upload

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,9 +2,7 @@ using FiniteDifferenceFormula
 using Test
 
 @testset "FiniteDifferenceFormula.jl" begin
-	# confirmed
 	@test sum(computecoefs(1, 0:1)[1]) == 0
-	@test sum(computecoefs(1, 0:2)[1]) == 0
 	@test sum(computecoefs(3, -2:5)[1]) == 0
 	@test computecoefs(3, -2:5)[2] > 0
 end
