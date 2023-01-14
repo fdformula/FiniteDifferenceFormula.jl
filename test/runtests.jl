1e6249688d46487795012fcde472e1bfddf30531
David Wang <64943188+Winux2k@users.noreply.github.com>
2023-01-14 18:20:05 -0500

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using FiniteDifferenceFormula
 using Test
 
 @testset "FiniteDifferenceFormula.jl" begin
-	@test sum(compute(1, 0:1)[1]) == 0
-	@test sum(compute(3, -2:5)[1]) == 0
-	@test compute(3, -2:5)[2] > 0
+	@test sum(compute(1, 0:1)[3]) == 0
+	@test sum(compute(3, -2:5)[3]) == 0
+	@test compute(3, -2:5)[4] > 0
 end
